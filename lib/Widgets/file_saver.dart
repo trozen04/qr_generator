954db8600f23ca26de40dfd15e64f2e42e1069cd
Bhoopendra <Bhoopendra>
2025-07-09 16:58:58 +0530

    Remove manage file permission and added save to gallery feature.

--- lib/Widgets/file_saver.dart
+++ lib/Widgets/file_saver.dart
@@ -1,60 +1,26 @@
-import 'dart:io';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/services.dart';
 
 import 'CustomSnackbar.dart';
 
 class FileSaver {
+  static const MethodChannel _channel = MethodChannel('com.trozen.qr_code_generator/filesaver');
+
   static Future<void> saveFile({
     required BuildContext context,
     required Uint8List bytes,
     String? fileName,
   }) async {
-    bool granted = false;
-
-    if (Platform.isAndroid) {
-      // Try request MANAGE_EXTERNAL_STORAGE permission explicitly first
-      if (!await Permission.manageExternalStorage.isGranted) {
-        var status = await Permission.manageExternalStorage.request();
-        if (!status.isGranted) {
-          if (status.isPermanentlyDenied) {
-            await openAppSettings();
-          }
-          CustomSnackbar.show(context, message: 'Storage permission denied', isSuccess: false);
-          return;
-        }
-      }
-      // granted if we reached here
-      granted = true;
-    } else {
-      // iOS or others
-      granted = true;
-    }
-
-
-    if (!granted) {
-      CustomSnackbar.show(context,
-          message: 'Storage permission denied', isSuccess: false);
-      return;
-    }
-
     try {
-      // Save inside your app external storage directory to avoid problems
-      final directory = await getExternalStorageDirectory();
-      final path = directory?.path ?? '/storage/emulated/0/Download';
+      final result = await _channel.invokeMethod('saveImageToGallery', {
+        'fileName': fileName ?? 'qr_code_${DateTime.now().millisecondsSinceEpoch}.png',
+        'bytes': bytes,
+      });
 
-      final file = File(
-        '$path/${fileName ?? 'qr_code_${DateTime.now().millisecondsSinceEpoch}.png'}',
-      );
-
-      await file.writeAsBytes(bytes);
-      CustomSnackbar.show(context,
-          message: 'File saved at: ${file.path}', isSuccess: true);
+      CustomSnackbar.show(context, message: result ?? 'Saved!', isSuccess: true);
     } catch (e) {
-      CustomSnackbar.show(context,
-          message: 'Failed to save file: $e', isSuccess: false);
+      CustomSnackbar.show(context, message: 'Failed to save file: $e', isSuccess: false);
     }
   }
 }
